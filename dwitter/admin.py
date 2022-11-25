ff23ffcfbbca0b926c0ce0ff43dd60c0a8ca4e02
rising-star92 <ryan.service.1001@gmail.com>
2022-11-25 12:21:38 -0500

    build social app with django

--- dwitter/admin.py
+++ dwitter/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.contrib.auth.models import User, Group
-from .models import Profile
+from .models import Dweet, Profile
 
 class ProfileInLine(admin.StackedInline):
     model = Profile
@@ -13,5 +13,5 @@ class UserAdmin(admin.ModelAdmin):
 admin.site.unregister(User)
 admin.site.register(User, UserAdmin)
 admin.site.unregister(Group)
-# admin.site.register(Profile)
+admin.site.register(Dweet)
 
--- dwitter/models.py
+++ dwitter/models.py
@@ -5,6 +5,22 @@ from django.dispatch import receiver
 
 # Create your models here.
 
+class Dweet(models.Model):
+    user = models.ForeignKey(
+        User,
+        related_name="dweets",
+        on_delete=models.DO_NOTHING
+    )
+    body = models.CharField(max_length=140)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return (
+            f"{self.user} "
+            f"({self.created_at:%Y-%m-%d %H:%M}): "
+            f"{self.body[:30]}..."
+        )
+
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     follows = models.ManyToManyField(
