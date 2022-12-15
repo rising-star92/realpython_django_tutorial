2171ece2e53c66b022d778ad242f7155b9596d2b
rising-star92 <ryan.service.1001@gmail.com>
2022-12-15 11:57:39 -0500

    add flesh card game app

--- social/settings.py
+++ social/settings.py
@@ -37,8 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.humanize',
     "dwitter",
-    "portfolio"
+    "portfolio",
+    "feedback",
+    "cards"
 ]
 
 MIDDLEWARE = [
--- social/urls.py
+++ social/urls.py
@@ -17,6 +17,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
+    path("", include("cards.urls")),
     path("feedback/", include("feedback.urls")),
     path("dwitter/", include("dwitter.urls")),
     path("portfolios/", include("portfolio.urls")),
