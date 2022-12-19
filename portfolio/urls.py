d330d7153e30c799b06820174c748088bb56593e
rising-star92 <ryan.service.1001@gmail.com>
2022-12-19 11:56:01 -0500

    fix the issues

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -1,7 +1,17 @@
 from django.urls import path
 from . import views
 
+app_name = "portfolio"
+
 urlpatterns = [
-    path("", views.portfolio_index, name='portfolio_index'),
-    path("<int:pk>/", views.portfolio_detail, name='portfolio_detail'),
+    path(
+        "",
+        views.portfolio_index,
+        name='portfolio_index'
+    ),
+    path(
+        "<int:pk>/",
+        views.portfolio_detail,
+        name='portfolio_detail'
+    ),
 ]
